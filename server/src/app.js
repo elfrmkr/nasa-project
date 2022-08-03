85d7b0c09f3df281e22b3059966569ef20e6ffb0
elfrmkr <elfrmkr98@gmail.com>
2022-08-03 16:53:59 +0300

    feat: serving frontend in production using build

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 
 const app = express();
@@ -11,6 +12,11 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
 
+app.get("/", (res, req) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 module.exports = app;
