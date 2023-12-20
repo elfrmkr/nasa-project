116da6d04695388535f4c11fd9fc859cbf90a8c2
Elif İrem Kara Kadyrov <83959541+elfrmkr@users.noreply.github.com>
2023-12-20 10:44:33 +0100

    feat: added mongoose to the system

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,28 @@
+
+// D5rjGrVUqEadzEaz
+
 const app = require("./app");
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 8000;
 const http = require("http");
+
+const MONGO_URL = 'mongodb+srv://nasa-api:D5rjGrVUqEadzEaz@cluster0.skrygwa.mongodb.net/nasa?retryWrites=true&w=majority'
 
 const { loadPlanetsData } = require("./models/planets.model");
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log("MongoDB connetion is ready!")
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err)
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL)
   return await loadPlanetsData();
 }
 
