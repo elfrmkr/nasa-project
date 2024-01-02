f3f4210e7e7a843cfc6b7abb14574fbf22b7f322
Elif İrem Kara Kadyrov <83959541+elfrmkr@users.noreply.github.com>
2024-01-02 14:48:54 +0100

    fix: adjust tests to mongo

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,11 +1,23 @@
 const request = require('supertest');
 const app = require('../../app');
-
+const {
+    mongoConnect,
+    mongoDisconnect
+} = require('../../services/mongo')
 const {
     loadPlanetsData,
 } = require('../../models/planets.model');
 
 describe('Launches API', () => {
+
+    beforeAll(async () => {
+        await mongoConnect();
+        await loadPlanetsData();
+    });
+
+    afterAll(async () => {
+        await mongoDisconnect();
+    });
 
     describe('Test GET /launches', () => {
         test('It should respond with 200 success', async () => {
