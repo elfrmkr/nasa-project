ccff4fcff7ce4c2c80247fb0e16c7319a679557d
Elif İrem Kara Kadyrov <83959541+elfrmkr@users.noreply.github.com>
2024-01-02 10:46:05 +0100

    refactor: planets are added to the mongodb

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -2,7 +2,7 @@ const { parse } = require("csv-parse");
 const path = require("path");
 const fs = require("fs");
 
-const habitablePlanets = [];
+const planets = require('./planets.mongo')
 
 function isHabitablePlanet(planet) {
   return (
@@ -25,26 +25,46 @@ function loadPlanetsData() {
           columns: true,
         })
       )
-      .on("data", (data) => {
+      .on("data", async (data) => {
         if (isHabitablePlanet(data)) {
-          habitablePlanets.push(data);
+          //insert + update = upsert
+          savePlanet(data)
         }
       })
       .on("error", (err) => {
         console.log(err);
         reject(err);
       })
-      .on("end", () => {
-        console.log(`${habitablePlanets.length} habitable planets found!`);
+      .on("end", async () => {
+        const countPlanetsFound = (await getAllPlanets()).length
+        console.log(`${countPlanetsFound} habitable planets found!`);
         resolve();
       });
   });
 }
-function getAllPlanets() {
-  return habitablePlanets;
+async function getAllPlanets() {
+  console.log(await planets.find({}))
+  return await planets.find({});
+}
+
+async function savePlanet(planet) {
+  try {
+    await planets.updateOne({
+      keplerName: planet.kepler_name,
+    },
+      {
+        keplerName: planet.kepler_name,
+      },
+      {
+        upsert: true,
+      });
+  } catch (err) {
+    console.error(`Could not save planet ${err}`);
+  }
 }
 
 module.exports = {
   loadPlanetsData,
   getAllPlanets,
 };
+
