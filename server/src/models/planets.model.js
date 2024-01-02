08e0033afbfb7ed600dbb3484806c63e5a03182d
Elif İrem Kara Kadyrov <83959541+elfrmkr@users.noreply.github.com>
2024-01-02 10:59:12 +0100

    refactor: exclude id and version from the response of mongo planets collection

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -43,8 +43,11 @@ function loadPlanetsData() {
   });
 }
 async function getAllPlanets() {
-  console.log(await planets.find({}))
-  return await planets.find({});
+  return await planets.find({},
+    {
+      '_id': 0,
+      '__v': 0
+    });
 }
 
 async function savePlanet(planet) {
